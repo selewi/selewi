033b0db7a764430ab43ed3a8e497a2e0e381fd2b
Tomás Esconjaureguy <esconjaureguy470@hotmail.com>
2018-10-12 12:44:27 -0300

    Transition

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(() => {
-	$('[data-toggle="tooltip"]').tooltip()
+	$('[data-toggle="tooltip"]').tooltip();
+
+	$('.btn-transition').click((e) => {
+		e.preventDefault ();
+		
+		$('body').addClass ('fade-out');
+
+		setTimeout(function(){
+		    window.location = $(e.target).attr("href");
+		}, 450);
+	});
 });
