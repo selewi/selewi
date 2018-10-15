da6c5d754963504ea4d83256c943f7cc66f74252
Tomás Esconjaureguy <esconjaureguy470@hotmail.com>
2018-10-14 23:17:58 -0300

    Update js/nav.js

--- js/nav.js
+++ js/nav.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
     let content = $('link[rel="import"]').get(0).import;
     let el = $(content).find('nav').get(0);
-    let navContainer = $('navbar');
+    let navContainer = $('#navbar');
 
     $('#navbar').append(el.cloneNode(true));
     $(`#${navContainer.attr('active')}`).addClass('active');
