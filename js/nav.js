3ba8a0a6290b59c96348687c791acaf17842ff0e
Tomás Esconjaureguy <esconjaureguy470@hotmail.com>
2018-10-14 23:08:11 -0300

    Test 3

--- js/nav.js
+++ js/nav.js
@@ -1,6 +1,6 @@
 $(document).ready(() => {
     let content = $('link[rel="import"]').import;
-    let el = $('nav');
+    let el = $('nav').get(0);
     let navContainer = $('navbar');
 
     $('navbar').append(el.cloneNode(true));
