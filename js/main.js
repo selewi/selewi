f4866dee7d10dd938ed261d15b66e2781e237614
Tomás Esconjaureguy <esconjaureguy470@hotmail.com>
2018-10-16 12:30:10 -0300

    Lazy load
    Image fade in on load.

--- js/main.js
+++ js/main.js
@@ -10,9 +10,16 @@ $(document).ready(() => {
 		
 		$('body').addClass ('fade-out');
 
-		setTimeout(function(){
+		setTimeout(() => {
 			$('body').addClass ('d-none');
 		    window.location = $(e.target).attr("href");
 		}, 450);
+	});
+
+	$('img[data-src]').each ((i,img) => {
+		$(img).attr ('src',$(img).attr('data-src'));
+		$(img).ready (() => {
+			$(img).removeAttr('data-src');
+		});
 	});
 });
