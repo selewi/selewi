98b727493abf66f656e477e01e724b1499039c6f
Tomás Esconjaureguy <esconjaureguy470@hotmail.com>
2019-04-16 10:20:57 -0300

    Auto birthday

--- js/main.js
+++ js/main.js
@@ -29,5 +29,18 @@ $(document).ready(() => {
 			$(img).removeAttr('data-src');
 		});
 	});
+
+	$('#age-span').text (getAge("4/20/1996"));
 });
 
+function getAge(DOB) {
+    var today = new Date();
+    var birthDate = new Date(DOB);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age = age - 1;
+    }
+
+    return age;
+}
